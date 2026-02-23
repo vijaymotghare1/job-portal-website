92cbe214c11bcc0553c77671a5f89434affb0057
vijaymotghare1 <motgharevijay707gmail.com>
2026-02-23 17:17:47 +0530

    job feature created to apply

---
+++ jobs.php
@@ -0,0 +1,10 @@
+<?php
+$jobs = [
+    ["id"=>1,"title"=>"Frontend Developer","company"=>"TechSoft","location"=>"Mumbai","salary"=>"6 LPA"],
+    ["id"=>2,"title"=>"Backend Developer","company"=>"CodeWorks","location"=>"Pune","salary"=>"8 LPA"],
+    ["id"=>3,"title"=>"UI/UX Designer","company"=>"PixelLab","location"=>"Bangalore","salary"=>"5 LPA"],
+    ["id"=>4,"title"=>"Data Analyst","company"=>"DataPro","location"=>"Delhi","salary"=>"7 LPA"],
+    ["id"=>5,"title"=>"PHP Developer","company"=>"WebCore","location"=>"Hyderabad","salary"=>"6.5 LPA"],
+    ["id"=>6,"title"=>"Full Stack Developer","company"=>"DevHub","location"=>"Chennai","salary"=>"9 LPA"]
+];
+?>
